c33d608426a0f03e09ddbe30844917f81fa04eb6
preciousnwaoha <nwaohaprecious25@gmail.com>
2022-03-26 15:32:58 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Heading } from "./components/Heading";
+import { Status } from "./components/Status";
+import { Oscar } from "./components/Oscar"
+import { Greet } from "./components/Greet";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Status status="loading" />
+      <Heading>Placeholder Text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicpariol</Heading>
+        <div className=""></div>
+      </Oscar>
+      <Greet name="Precious" isLoggedIn={false} />
     </div>
   );
 }
