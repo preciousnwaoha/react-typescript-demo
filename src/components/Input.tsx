b113768d12e41225624b1fe1004e28a00cc3e576
preciousnwaoha <nwaohaprecious25@gmail.com>
2022-03-26 16:18:09 +0100

    Props Types and Reusability tips

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,6 +3,9 @@ type InputProps = {
     handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const Input = (props:InputProps) => {
-    return <input type="text" value={props.value} onChange={props.handleChange} />
+export const Input = ({value, handleChange}: InputProps) => { // destructuring props
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event)
+    }
+    return <input type="text" value={value} onChange={handleChange} />
 }
