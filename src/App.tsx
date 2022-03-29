52b806bcbc20b175103f03c43f437b93e0faa380
preciousnwaoha <nwaohaprecious25@gmail.com>
2022-03-29 20:36:04 +0100

    Template Literal Types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import { RandomNumber } from "./components/restriction/RandomNumber"
+import { Toast } from "./components/templateLiterals/Toast";
+
 
 function App() {
   return (
     <div className="App">
-      <RandomNumber value={10} isPositive />
+      <Toast position="center" />
     </div>
   );
 }
---
+++ src/components/templateLiterals/Toast.tsx
@@ -0,0 +1,12 @@
+type HorizontaPosition = "left" | "center" | "right";
+type VerticalPosition = "top" | "center" | "bottom";
+
+type ToastProps = {
+  position:
+    | Exclude<`${HorizontaPosition}-${VerticalPosition}`, "center-center">
+    | "center";
+};
+
+export const Toast = ({ position }: ToastProps) => {
+  return <div>Toast Notification Position - {position}</div>;
+};
